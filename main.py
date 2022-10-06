3d98f32e6aa52e639e40211feac4030c949a3057
Khaled Alghanim <al.ghanim@me.com>
2022-10-06 12:01:11 +0300

    Enhancements

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 ########  Sudoku Solver ########
 
 board = [
-    [5,3,0,0,7,0,0,0,0],
-    [6,0,0,1,9,5,0,0,0],
-    [0,9,8,0,0,0,0,6,0],
-    [8,0,0,0,6,0,0,0,3],
-    [4,0,0,8,0,3,0,0,1],
-    [7,0,0,0,2,0,0,0,6],
-    [0,6,0,0,0,0,2,8,0],
-    [0,0,0,4,1,9,0,0,5],
-    [0,0,0,0,8,0,0,7,9]
+    [4,3,7,0,0,0,0,9,0],
+    [0,8,0,4,0,9,1,3,0],
+    [0,0,0,0,0,0,7,0,4],
+    [8,0,5,9,1,0,0,0,0],
+    [2,4,3,0,0,7,9,1,5],
+    [7,9,1,2,0,4,0,0,0],
+    [6,0,0,3,0,0,0,7,0],
+    [1,5,0,0,2,0,3,4,0],
+    [0,0,4,0,0,0,2,5,0]
 ]
 
 
-# Printing the board
+# This function for printing the board
 def printBoard (board) :
     print(f"\n| {board[0][0]}  {board[0][1]}  {board[0][2]} | {board[0][3]}  {board[0][4]}  {board[0][5]} | {board[0][6]}  {board[0][7]}  {board[0][8]} |")
     print(f"\n| {board[1][0]}  {board[1][1]}  {board[1][2]} | {board[1][3]}  {board[1][4]}  {board[1][5]} | {board[1][6]}  {board[1][7]}  {board[1][8]} |")
@@ -27,7 +27,6 @@ def printBoard (board) :
     print(f"\n| {board[7][0]}  {board[7][1]}  {board[7][2]} | {board[7][3]}  {board[7][4]}  {board[7][5]} | {board[7][6]}  {board[7][7]}  {board[7][8]} |")
     print(f"\n| {board[8][0]}  {board[8][1]}  {board[8][2]} | {board[8][3]}  {board[8][4]}  {board[8][5]} | {board[8][6]}  {board[8][7]}  {board[8][8]} |")
     print("_______________________________")
-
 
 def isNumberInRow (board, number, row):
     for index in range(9):
@@ -57,26 +56,24 @@ def IsValidPlacement (board, number, row, column) :
 
 number = IsValidPlacement(board, 1, 0, 2)
 
-# print(f"IsValidPlacement = {number}")
+
 
 def solveBoard (board):
     for i in range(9):
-        print(f"i = {i}")
         for j in range(9):
-            print(f"j = {j}")
             if board[i][j] == 0 :
-                print(f"checking 0 field in location : row {i+1} and column {j+1}")
                 for number in range(1,10):
-                    print(f"number is {number}")
                     if IsValidPlacement(board, number, i, j):
-                        print(f"The number {number} is valid in ")
                         board[i][j] = number
                         if solveBoard (board):
                             return True
                         else:
                             board[i][j] = 0
+                return False
+    return True
 
 
-solveBoard (board)
+print("Solved Successfully!") if solveBoard(board) else print("Unsolvable Board!")
 
+# Print the board after solving it.
 printBoard(board)
